5ae5d798c3d8abdc088af59fa3a6496c6984c998
RoySaudamini <saudaminiroy17@gmail.com>
2024-10-25 15:25:44 +0530

    Add files via upload

---
+++ contacts.js
@@ -0,0 +1,34 @@
+document.getElementById('linkedin').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/saudamini-roy-a19412335');
+});
+
+document.getElementById('x').addEventListener('click', function() {
+    window.open('https://x.com/RoySaudamini');
+});
+
+document.getElementById('github').addEventListener('click', function() {
+    window.open('https://github.com/RoySaudamini');
+});
+
+
+// links for contact adress page
+document.getElementById('email2').addEventListener('click', function() {
+    window.location.href = 'mailto:saudamini.roy2024@uem.edu.in';
+});
+
+
+document.getElementById('tel2').addEventListener('click', function() {
+    window.location.href = 'tel:+91 99074 70771';
+}); 
+
+document.getElementById('linkedin2').addEventListener('click', function() {
+    window.open('https://www.linkedin.com/in/saudamini-roy-a19412335');
+});
+
+document.getElementById('x2').addEventListener('click', function() {
+    window.open('https://x.com/RoySaudamini');
+});
+
+document.getElementById('github2').addEventListener('click', function() {
+    window.open('https://github.com/RoySaudamini');
+});
---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById('education-dev').addEventListener('click', function() {
+    document.getElementById('education').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('events-dev').addEventListener('click', function() {
+    document.getElementById('eventsAttended').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('membership-dev').addEventListener('click', function() {
+    document.getElementById('membership').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.getElementById('contact-dev').addEventListener('click', function() {
+    document.getElementById('contacts').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
